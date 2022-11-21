260114efc4d048707ea0fe8676ac2dc3dc30b179
1101 <32242762+A-Dying-Pig@users.noreply.github.com>
2022-11-20 19:10:43 -0800

    Update 2022_5_20.js

--- assets/js/2022_5_20.js
+++ assets/js/2022_5_20.js
@@ -373,7 +373,7 @@ function heart_2022_5_20() {
     .attr("stroke-dashoffset", 0);
 
     //text
-    txt = ["Love", "&#128151;", "兜兜"]
+    txt = ["Love", "&#128151;", "Mi"]
     svg.selectAll("text")
     .data(txt)
     .enter().append("text")
